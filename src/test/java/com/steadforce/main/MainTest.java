d0914692d7b355df26dfc592bcbf445191f1b36a
Daniel Roeder (ext. ) <daniel.roeder@steadforce.com>
2018-11-27 09:40:42 +0100

    clear out

--- src/test/java/com/steadforce/main/MainTest.java
+++ src/test/java/com/steadforce/main/MainTest.java
@@ -3,46 +3,20 @@ package com.steadforce.main;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-
 public class MainTest {
 
     private static Main m;
 
-    @BeforeAll
-    public static void setUp(){
+    @BeforeAll public static void setUp() {
         m = new Main();
     }
 
-
-    @Test
-    public void testMainDoSomeTrue() {
-        assertTrue(m.doSome(true), "expected true");
-    }
-
-
-    @Test
-    public void testMainDoSomeFalse() {
-        assertFalse(m.doSome(false), "expected false");
-    }
-
-
-    @Test
-    public void testMainDoSomeFalse_wrong() {
-//        assertTrue(m.doSome(false), "expected false");
-    }
-
-
-    @Test
-    public void testMainDoFail() {
-//        m.doFail();
-    }
-
-    @Test
-    public void testMainDoNOTFail() {
+    /**
+     * I'm some doc
+     *
+     * @see Main
+     */
+    @Test public void testMain() {
 
     }
 }
--- src/test/java/com/steadforce/other/OtherTest.java
+++ src/test/java/com/steadforce/other/OtherTest.java
@@ -1,28 +1,10 @@
 package com.steadforce.other;
 
-import com.steadforce.other.Other;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OtherTest {
 
     @Test
-    public void testOtherSomeTrue(){
-        assertTrue(new Other().doSome(true));
+    public void testOther(){
     }
-
-
-    @Test
-    public void testOtherSomeFalse(){
-        assertFalse(new Other().doSome(false));
-    }
-
-
-    @Test
-    public void testOtherOther(){
-        assertFalse(false);
-    }
-
 }
